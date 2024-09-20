09ee304a59edb450cf607961bfb6287f334ea7cd
nirmalkumar1212 <nirmalkumar12.m@gmail.com>
2024-09-20 17:36:57 +0530

    Added APpium activity

--- Appium/Activities/Activity1.java
+++ Appium/Activities/Activity1.java
@@ -1,49 +1,88 @@
-package activities;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.openqa.selenium.By;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+package Activities;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Activity1 {
-	AndroidDriver driver;
+    
+    AndroidDriver driver;
 
-	@BeforeClass
-	public void setup() throws MalformedURLException {
-		UiAutomator2Options options = new UiAutomator2Options();
-		options.setPlatformName("android");
-		options.setAutomationName("UiAutomator2");
-		options.setAppPackage("com.google.android.calculator");
-		options.setAppActivity("com.android.calculator2.Calculator");
-		options.noReset();
+  
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("android");
+        options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.android.calculator2");
+        options.setAppActivity(".Calculator");
+        options.noReset();
+        
+        URL serverURL = new URL("http://localhost:4723/");
 
-		URL serverUrl = new URL("http://localhost:4723/");
+        driver = new AndroidDriver(serverURL, options);
+    }
 
-		driver = new AndroidDriver(serverUrl, options);
-	}
+    // Test method
+    @Test
+    public void multiplyTest() {
+        
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+        driver.findElement(AppiumBy.id("digit_8")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
 
-	@Test
-	public void multiplyCase() {
-		driver.findElement(AppiumBy.accessibilityId("9")).click();
-		driver.findElement(AppiumBy.accessibilityId("multiply")).click();
-		driver.findElement(AppiumBy.accessibilityId("5")).click();
-		driver.findElement(AppiumBy.accessibilityId("equals")).click();
+	// Find the result
+        String result = driver.findElement(AppiumBy.id("result")).getText();
 
-		String result = driver.findElement(AppiumBy.id("result_final")).getText();
+	// Assertion
+        Assert.assertEquals(result, "40");
+    }
+@Test
+    public void multiplyTestLocators() {
+        // Perform the calculation
+     
+        driver.findElement(AppiumBy.xpath("//android.widget.Button[contains(@resource-id, 'digit5') AND (@text = '5')]")).click();
+        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+        driver.findElement(AppiumBy.id("digit_8")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+ 
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result_final")).getText();
+ 
+        // Assertion
+        Assert.assertEquals(result, "40");
+    }
+    
+    @Test
+    public void listElements() {
+        // Wait for elements to load
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+            AppiumBy.className("android.webkit.WebView")
+        ));
+ 
+        // Find all text elements on the page
+        List<WebElement> textItems = driver.findElements(
+            AppiumBy.xpath("//android.view.View/android.widget.Button")
+        );
+        System.out.println(textItems.size());
+        for (WebElement textItem : textItems) {
+            System.out.println(textItem.getText());
+        }
+    }
 
-		Assert.assertEquals(result, "45");
-	}
 
-	@AfterClass
-	public void tearDown() {
-		driver.quit();
-	}
+    // Tear down method
+    @AfterClass
+    public void tearDown() {
+        // Close the app
+        driver.quit();
+    }
 }
